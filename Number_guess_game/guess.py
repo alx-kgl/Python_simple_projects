f8f896f74c2e182d3244b8c91125a3f7133eefc8
ishimwezachee <zacheeishimwe@gmail.com>
2023-01-05 22:12:09 +0200

    add instruction for the projects

---
+++ Number_guess_game/guess.py
@@ -0,0 +1,11 @@
+# Number Guessing game  
+
+# Range shall between 0 - 100   
+
+# Use random number between 0 - 100
+
+# Number of guessing should be 5 times 
+
+# You should use higher or less to provide hint to the user 
+
+# Once the user found the result, the system should stop asking=> Congrats you’ve successful found the number. 
