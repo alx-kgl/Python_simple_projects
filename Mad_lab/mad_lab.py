3c9ce135f52eb83eaa7abc14f2004298068d1f58
ishimwezachee <zacheeishimwe@gmail.com>
2023-01-05 21:50:44 +0200

    push changes

---
+++ Mad_lab/mad_lab.py
@@ -0,0 +1,30 @@
+
+
+adjective1 = input("Type an adjective: ")
+noun1  = input("Type a noun: ")
+verb_past_tense = input("Type a verb in past tense: ")
+adverb = input("Type an adverb: ")
+adjective2 = input("Type an adjective: ")
+noun2 = input("Type a noun: ")
+noun3 = input("Type a noun: ")
+adjective3 = input("Type an adjective: ")
+verb2 = input("Type a verb: ")
+adverb3 = input("type an adverb: ")
+verb_past_tense2 = input("Type an adjective: ")
+adjective4 = input("Type an adjective: ")
+
+
+
+print(f"""Today I went to the zoo. I saw a(n)
+{adjective1}
+{noun1} jumping up and down in its tree.
+He {verb_past_tense} {adverb}
+through the large tunnel that led to its {adjective2}
+{noun2} I got some peanuts and passed
+them through the cage to a gigantic gray {noun3}
+towering above my head. Feeding that animal made
+me hungry. I went to get a {adjective3} scoop
+of ice cream. It filled my stomach. Afterwards I had to
+{verb2} {adverb3} to catch our bus.
+When I got home I {verb_past_tense2} my
+mom for a {adjective4} day at the zoo. """)
